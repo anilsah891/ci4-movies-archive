41fa2761c3de7bcfb8d4acb25f08c7f47568b1a5
anilsah891 <anilsah891@gmail.com>
2025-03-31 05:23:58 +0100

    Removed duplicate ci4-app nesting

--- ci4-app/app/Views/apis/wikipedia.php
+++
@@ -1,5 +0,0 @@
-<?php foreach($links->query->exturlusage->eu as $link): ?>
-    <h2><?=$link['title']?></h2>
-    <p>Links to: <a href='<?=$link['url']?>'><?=$link['url']?></a></p>
-    <p>Page ID: <?=$link['pageid']?></p>
-<?php endforeach; ?>
